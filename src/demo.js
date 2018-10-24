62b633c167ab9cc603c73ec75b90096a93a51632
ottonove <33423293+ottonove@users.noreply.github.com>
2018-10-24 11:51:04 +0900

    Initial commit

---
+++ src/demo.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  tab: {
+    width:"30%",
+  }
+};
+
+class CenteredTabs extends React.Component {
+  state = {
+    value: 0,
+  };
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <Paper className={classes.root}>
+        <Tabs
+          value={this.state.value}
+          onChange={this.handleChange}
+          indicatorColor="primary"
+          textColor="primary"
+          centered
+        >
+          <Tab className={classes.tab} label="栄養１" />
+          <Tab className={classes.tab} label="Item Two" />
+          <Tab className={classes.tab} label="Item Three" />
+        </Tabs>
+      </Paper>
+    );
+  }
+}
+
+CenteredTabs.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(CenteredTabs);
